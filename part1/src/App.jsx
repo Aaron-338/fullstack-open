a7fa63e2e66d04227be71fd171b3dc53142c6b5a
Aaron <mohaumosotho586@gmail.com>
2026-01-07 21:59:09 +0200

    1.4 done

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -3,48 +3,47 @@ const Header = (props) => {
 }
 
 const Part = (props) => {
-  return (<p>{props.part?.name} {props.part?.exercises}</p>) // Optional chaining to avoid errors if part is undefined
+  return (<p>{props.part.name} {props.part.exercises}</p>)
 }
 
 const Content = (props) => {
   return (
     <div>
-      <Part
-        part={props.part}
-      />
+      {props.parts.map((part, index) => ( //Each part object is transformed into a Part component
+        <Part key={index} part={part} />
+      ))}
     </div>
   )
 }
 
-
-
-const Total = (props) => {
-  return (<p>Number of exercises {props.excercises1 + props.excercises2 + props.excercises3}</p>)
+const Total = (props) => { 
+  return (<p>Number of exercises {props.parts.reduce((sum, part) => sum + part.exercises, 0)}</p>)
 }
 
 const App = () => { 
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
-  return (<div>
-      <Header course = {course}/>
-      <Content part = {part1} />
-      <Content part = {part2}/>
-      <Content part = {part3} />
-      <Total excercises1 = {part1.exercises} excercises2 = {part2.exercises} excercises3 = {part3.exercises}/>
-  </div>)
-    
+  return (
+    <div>
+      <Header course={course}/>
+      <Content parts={parts} />
+      <Total parts={parts}/>
+    </div>
+  )
 }
 
 export default App
