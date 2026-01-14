bcde62e2c0d936f2ad99d30701dcf33757f87bb5
Aaron <mohaumosotho586@gmail.com>
2026-01-14 22:04:09 +0200

    2.11 done

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Contact = ({person}) => <div>{person.name} {person.number}</div>
 
@@ -19,17 +20,23 @@ const Filter = ({filter,handleFilterChange}) => (
 )
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
-
+  
+  const hook = () => {
+  console.log('effect')
+  axios
+    .get('http://localhost:3001/persons')
+    .then(response => {
+      console.log('promise fulfilled')
+      setPersons(response.data)
+    })
+  }
+  useEffect(hook, [])
+  console.log('render', persons.length, 'persons')
   const addPerson = (event) => {
     console.log("button clicked")
     event.preventDefault()
@@ -68,22 +75,22 @@ const App = () => {
     : persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
 
   return ( 
-    <div>
-      <h2>Phonebook</h2>
-      <Filter filter={filter} handleFilterChange={handleFilterChange} />
-      
-      <h3>Add a new</h3>
-      <PersonForm 
-        newName={newName}
-        newNumber={newNumber}
-        addPerson={addPerson}
-        handleNameChange={handleNameChange}
-        handleNumberChange={handleNumberChange}
-      />
-      
-      <h3>Numbers</h3>
-      <Persons persons={personsToShow} />
-    </div>
+      <div>
+        <h2>Phonebook</h2>
+        <Filter filter={filter} handleFilterChange={handleFilterChange} />
+        
+        <h3>Add a new</h3>
+        <PersonForm 
+          newName={newName}
+          newNumber={newNumber}
+          addPerson={addPerson}
+          handleNameChange={handleNameChange}
+          handleNumberChange={handleNumberChange}
+        />
+        
+        <h3>Numbers</h3>
+        <Persons persons={personsToShow} />
+      </div>
   )
 }
 
