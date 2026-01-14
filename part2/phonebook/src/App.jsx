d89724f2fcae1c90e268bf5ebfb39245dcaebce8
Aaron <mohaumosotho586@gmail.com>
2026-01-14 19:46:40 +0200

    phonebook completed

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,25 +1,51 @@
 import { useState } from 'react'
 
+const Contact = ({person}) => <div>{person.name} {person.number}</div>
+
+const Persons = ({persons}) => persons.map((person,index) => <Contact person={person} key={index}></Contact>)
+
+const PersonForm = ({newName,newNumber,addPerson,handleNameChange,handleNumberChange}) => {
+  return (
+    <form onSubmit={addPerson}>
+      <div>name: <input value={newName} onChange={handleNameChange}/></div>
+      <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
+      <div><button type="submit">add</button></div>
+    </form>
+  )
+}
+
+const Filter = ({filter,handleFilterChange}) => (
+  <div>filter shown with <input value={filter} onChange={handleFilterChange}></input></div>
+)
+
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+  
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
   const addPerson = (event) => {
-    console.log("button clicked  ")
+    console.log("button clicked")
     event.preventDefault()
-    const newPerson = {name:newName}
-    if(persons.find(person => person.name.toLocaleLowerCase() === newName.toLocaleLowerCase()))
-    {
+    const newPerson = {name:newName , number:newNumber , id: persons.length+1}
+    
+    if(persons.find(person => person.name.toLowerCase() === newName.toLowerCase())) {
       alert(`${newName} is already added to phonebook`)
-      console.log("a duplicate was found")
+      console.log("a duplicate name was found")
       return
     }
+    
     setPersons(persons.concat(newPerson))
-    console.log("no duplicte was found")
-    persons.forEach((person) => console.log(person.name))
+    console.log("no duplicate name was found")
+    persons.forEach((person) => console.log(person.name, person.number, person.id))
     setNewName('')
+    setNewNumber('')
   }
 
   const handleNameChange = (event) => {
@@ -27,20 +53,36 @@ const App = () => {
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }  
+
+  const handleFilterChange = (event) => {
+    console.log(event.target.value)
+    setFilter(event.target.value)
+  }
+
+  const personsToShow = filter === '' 
+    ? persons 
+    : persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
+
   return ( 
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <div>debug: {newName}</div>
-      ...
+      <Filter filter={filter} handleFilterChange={handleFilterChange} />
+      
+      <h3>Add a new</h3>
+      <PersonForm 
+        newName={newName}
+        newNumber={newNumber}
+        addPerson={addPerson}
+        handleNameChange={handleNameChange}
+        handleNumberChange={handleNumberChange}
+      />
+      
+      <h3>Numbers</h3>
+      <Persons persons={personsToShow} />
     </div>
   )
 }
